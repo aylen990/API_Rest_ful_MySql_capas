421ec6ec30d8ed088b16d989f2b91125e8692b73
aylen <aylu660@gmail.com>
2025-10-20 00:51:14 -0300

    Actividad rutas productos

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,76 +1,69 @@
-import pool from '../../config/conexion.js'
-
-import * as model from '../models/users.models.js'
+import * as model from "../models/users.model.js";
 
 export const getAllUsers = async (req, res) => {
-    const rows = await model.getAllUsers()
-
-    if(rows.errno){
-        return res.status(500).send('ERROR, en consulta, cod:' + rows.errno)
-    }
-
-    res.json(rows)
-    res.status(201).send(`Usuario creado con id ${rows.insertId}`)
-}
-
-    // const sql = "SELECT * FROM users";
-    // try {
-    //     const connection = await pool.getConnection();
-    //     const [rows] = await connection.query(sql);
-    //     connection.release(); //libera conexion   
-    //     res.json(rows)
-    // } catch (error) {
-    //     // res.status(500).send('ERROR, no se pudo realizar la consulta')
-    // }
-// }
+  const rows = await model.getAllUsers();
+  if (rows.errno) {
+    return res
+      .status(500)
+      .send("ERROR, no se pudo realizar la consulta: " + rows.errno);
+  }
+  rows.length > 0
+    ? res.json(rows)
+    : res.status(404).send("No hay usuarios registrados");
+};
 
 export const getUserById = async (req, res) => {
-    const id = parseInt(req.params.id)
-    console.log(id)
-    ikol(rows[0]) ? res.json(rows[0]) : res.send('El usuario no existe')
-}
+  const id = parseInt(req.params.id);
+  const rows = await model.getUserById(id);
+
+  if (rows.errno) {
+    return res
+      .status(500)
+      .send("ERROR, no se pudo realizar la consulta: " + rows.errno);
+  }
+
+  rows[0] ? res.json(rows[0]) : res.status(404).send("El usuario no existe");
+};
 
 export const createUser = async (req, res) => {
-    const values = req.body
-    const rows = await model.createUser(values)
-
-    if(rows.errno){
-        return res.status(500).send('ERROR, en consulta, cod:' + rows.errno)
-    }
-
-    res.json(rows)
-    res.status(201).send(`Usuario creado con id ${rows.insertId}`)
-    // const sql = "INSERT INTO users (Name, Email, Image, Pass, Type_user) VALUES (?,?,?,?,?)";
-
-}
+  const values = req.body;
+  const rows = await model.createUser(values);
+  if (rows.errno) {
+    return res
+      .status(500)
+      .send("ERROR, no se pudo realizar la consulta: " + rows.errno);
+  }
+  res.status(201).send("Usuario creado con el id: " + rows.insertId);
+};
 
 export const updateUser = async (req, res) => {
-    const id = req.params.id
-    const values = req.body
-    const rows = await model.updateUser(values, id)
+  const id = req.params.id;
+  const newValues = req.body;
 
-    if (rows.errno) {
-        return res.status(500).send("Error en consulta, cod: " + rows.errno)
-    }
+  const rows = await model.updateUser(newValues, id);
 
-    if (rows.affectedRows == 0) {
-        return res.send("Usuario no existe")
-    }
+  if (rows.errno) {
+    return res
+      .status(500)
+      .send("ERROR, no se pudo realizar la consulta: " + rows.errno);
+  }
 
-    return res.send("usuario actualizado")
-}
+  rows.affectedRows === 0
+    ? res.status(404).send("El usuario no existe")
+    : res.send("Datos actualizados");
+};
 
-export const deleteUser =  async (req, res) => {
-    const id = req.params.id
-    const sql = "DELETE FROM users WHERE ID_user = ?";
-    try {
-        const connection = await pool.getConnection();
-        const [rows] = await connection.query(sql, [id]);  //hay que pasale el sql y el dato que reemplaza el signo ?
-        connection.release();
-        // console.log(rows)
-        // //affectedRoews muestra la cant de registros actualizados si es igual a cero no modifico ningun registro
-        (rows.affectedRows == 0) ? res.send('Usuario no existe') : res.status(204).send()
-    } catch (error) {
-        res.status(500).send('ERROR, no se pudo realizar la consulta')
-    }
-} 
+export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  const rows = await model.deleteUser(id);
+
+  if (rows.errno) {
+    return res
+      .status(500)
+      .send("ERROR, no se pudo realizar la consulta: " + rows.errno);
+  }
+
+  rows.affectedRows === 0
+    ? res.status(404).send("El usuario no existe")
+    : res.send("Dato eliminado");
+};
